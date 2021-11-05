7ec31a6b1508eac45bfac93d289d4d1f02293014
Tommaso Passarotto <passarotto13@gmail.com>
2021-11-05 15:15:04 +0100

    pseudocodice

---
+++ main.js
@@ -0,0 +1,5 @@
+// 1. creiamo un array di oggetti per i post
+// 2. ricreiamo su js la struttura html per tutti i post
+    // 2.1 prima lo facciamo per uno
+    // 2.2 poi creiamo un ciclo per riprodurre ogni oggetto
+// 3. abilitiamo il tasto dei "mi piace" per aumentrne il conteggio
