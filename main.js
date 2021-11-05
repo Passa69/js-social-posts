70afa4e0f5947682268a52dd8864d3021b66d63b
Tommaso Passarotto <passarotto13@gmail.com>
2021-11-05 16:44:45 +0100

    aggiunta ciclo

--- main.js
+++ main.js
@@ -19,120 +19,119 @@ const post = [
     },
 
     {
-        "author"     : "",
-        "profileImg" : "",
-        "date"       : "",
-        "text"       : "",
-        "img"        : "",
-        "likesNumb"  : ""
+        "author"     : "Steve Roger",
+        "profileImg" : "https://unsplash.it/300/300?image=13",
+        "date"       : "3 mesi fa",
+        "text"       : "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "img"        : "https://unsplash.it/600/300?image=170",
+        "likesNumb"  : "70"
     },
 
     {
-        "author"     : "",
-        "profileImg" : "",
-        "date"       : "",
-        "text"       : "",
+        "author"     : "Tony Stark",
+        "profileImg" : "https://unsplash.it/300/300?image=10",
+        "date"       : "2 mesi fa",
+        "text"       : "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
         "img"        : "",
-        "likesNumb"  : ""
+        "likesNumb"  : "60"
     },
 
     {
-        "author"     : "",
-        "profileImg" : "",
-        "date"       : "",
-        "text"       : "",
-        "img"        : "",
-        "likesNumb"  : ""
+        "author"     : "Bruce Banner",
+        "profileImg" : "https://unsplash.it/300/300?image=9",
+        "date"       : "1 mese fa",
+        "text"       : "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "img"        : "https://unsplash.it/600/300?image=165",
+        "likesNumb"  : "100"
     }
 ]
+// ciclo
+for (i = 0; i < post.length; i++) {
+    // html
+    let postDiv = document.createElement("div");
+    postDiv.classList.add("post");
 
-// html
-let postDiv = document.createElement("div");
-postDiv.classList.add("post");
+    let postHead = document.createElement("div");
+    postHead.classList.add("post__header");
 
-let postHead = document.createElement("div");
-postHead.classList.add("post__header");
+    let postMt = document.createElement("div");
+    postMt.classList.add("post-meta");
 
-let postMt = document.createElement("div");
-postMt.classList.add("post-meta");
+    let postIcon = document.createElement("div");
+    postIcon.classList.add("post-meta__icon");
+    postIcon.innerHTML += `
+    <img 
+        class="profile-pic" 
+        src="${post[i].profileImg} " 
+        alt="Phil Mangione"
+    >`;
 
-let postIcon = document.createElement("div");
-postIcon.classList.add("post-meta__icon");
-postIcon.innerHTML += `
-<img 
-    class="profile-pic" 
-    src="${post[0].profileImg} " 
-    alt="Phil Mangione"
->`;
+    let postData = document.createElement("div");
+    postData.classList.add("post-meta__data");
+    postData.innerHTML += `
+    <div class="post-meta__author">
+        ${post[i].author}
+    </div>`;
+    postData.innerHTML += `
+    <div class="post-meta__time">
+        ${post[i].date}
+    </div>`;
 
-let postData = document.createElement("div");
-postData.classList.add("post-meta__data");
-postData.innerHTML += `
-<div class="post-meta__author">
-    ${post[0].author}
-</div>`;
-postData.innerHTML += `
-<div class="post-meta__time">
-    ${post[0].date}
-</div>`;
+    let postText = document.createElement("div");
+    postText.classList.add("post__text");
+    postText.innerText += `
+        ${post[i].text}
+    `;
 
-let postText = document.createElement("div");
-postText.classList.add("post__text");
-postText.innerText += `
-    ${post[0].text}
-`;
+    let postImg = document.createElement("div");
+    postImg.classList.add("post__image");
+    postImg.innerHTML += `
+    <img 
+        src="${post[i].img}" 
+        alt=""
+    >`;
 
-let postImg = document.createElement("div");
-postImg.classList.add("post__image");
-postImg.innerHTML += `
-<img 
-    src="${post[0].img}" 
-    alt=""
->`;
+    let postFoot = document.createElement("div");
+    postFoot.classList.add("post__footer");
 
-let postFoot = document.createElement("div");
-postFoot.classList.add("post__footer");
+    let postLike = document.createElement("div");
+    postLike.classList.add("likes");
+    postLike.classList.add("js-likes");
 
-let postLike = document.createElement("div");
-postLike.classList.add("likes");
-postLike.classList.add("js-likes");
+    let postCta = document.createElement("div");
+    postCta.classList.add("likes__cta");
+    postCta.innerHTML +=`
+    <a class="like-button  js-like-button" href="#" data-postid="1">
+        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+        <span class="like-button__label">Mi Piace</span>
+    </a>
+    `;
 
-let postCta = document.createElement("div");
-postCta.classList.add("likes__cta");
-postCta.innerHTML +=`
-<a class="like-button  js-like-button" href="#" data-postid="1">
-    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-    <span class="like-button__label">Mi Piace</span>
-</a>
-`;
+    let postCounter = document.createElement("div");
+    postCounter.classList.add("likes__counter");
+    postImg.innerHTML += `
+    Piace a 
+        <b id="like-counter-1" class="js-likes-counter">
+            ${post[i].likesNumb}
+        </b> 
+    persone`;
 
-// let postButton = document.createElement("a");
-// postButton.classList.add("like-button");
-// postButton.classList.add("js-like-button");
+    postDiv.appendChild(postHead);
+    postDiv.appendChild(postText);
 
-let postCounter = document.createElement("div");
-postCounter.classList.add("likes__counter");
-postImg.innerHTML += `
-Piace a 
-    <b id="like-counter-1" class="js-likes-counter">
-        ${post[0].likesNumb}
-    </b> 
-persone`;
+    postHead.appendChild(postMt);
 
-postDiv.appendChild(postHead);
-postDiv.appendChild(postText);
+    postMt.appendChild(postIcon);
+    postMt.appendChild(postData);
 
-postHead.appendChild(postMt);
+    postDiv.appendChild(postImg);
+    postDiv.appendChild(postFoot);
 
-postMt.appendChild(postIcon);
-postMt.appendChild(postData);
+    postFoot.appendChild(postCounter);
+    postFoot.appendChild(postLike);
 
-postDiv.appendChild(postImg);
-postDiv.appendChild(postFoot);
+    postLike.appendChild(postCta);
 
-postFoot.appendChild(postCounter);
-postFoot.appendChild(postLike);
+    postCont.appendChild(postDiv);
+}
 
-postLike.appendChild(postCta);
-
-postCont.appendChild(postDiv);
